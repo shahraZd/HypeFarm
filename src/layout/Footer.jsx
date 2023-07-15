319b51f062aa2c96a4d040cdb37201aba6bdf02d
sha <sheherazad.08@gmail.com>
2023-07-15 23:09:50 +0100

    fix footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,37 +1,29 @@
+import Image from "next/image";
+import logo from "pub/hypefarm-logo.svg";
 const Footer = () => {
   return (
-    <footer className=" footer bg-primary-300 w-full ">
-      <div className="footer_container bg-black">
-        <div className="footer_links">
-          <div>
-            <p className="footer__subtitle">Subscribe to Our newsletter</p>
+    <footer className=" rounded-lg shadow dark:bg-gray-900 m-4">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="flex items-center justify-between my-2">
+          <Image src={logo} alt="HypeFarm logo" width={150} height={150} />
 
-            <div className="footer__form">
-              <div className="blog-hero__form">
-                <div className="stc-subscribe-wrapper well"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="footer_copy">
-          <p>© 2017-2023 Ninjapromo.io</p>
-          <ul id="menu-footer-copy-menu" className="">
-            <li
-              id="menu-item-5266"
-              className="menu-item menu-item-type-post_type menu-item-object-page menu-item-5266"
-            >
-              <a href="https://ninjapromo.io/privacy-policy">Privacy Policy</a>
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <li>
+              <a href="#company" className="mr-4 hover:underline md:mr-6 ">About</a>
             </li>
-            <li
-              id="menu-item-5267"
-              className="menu-item menu-item-type-post_type menu-item-object-page menu-item-5267"
-            >
-              <a href="https://ninjapromo.io/delivery-policy">
-                Terms &amp; Conditions
-              </a>
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
             </li>
-          </ul>{" "}
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6 ">Licensing</a>
+            </li>
+            <li>
+              <a href="#contact" className="hover:underline">Contact</a>
+            </li>
+          </ul>
         </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-base text-center mt-2 text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" className="hover:underline">HypeFarm™</a>. All Rights Reserved.</span>
       </div>
     </footer>
   );
