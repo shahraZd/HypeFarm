2a6b9fe3ee60be917c291ae083c8a38c75f5407b
sha <sheherazad.08@gmail.com>
2023-08-22 01:23:50 +0100

    Hero animation fixes part 1

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -1,17 +1,19 @@
 // import BgHero from "../../../public/gradientHero/";
+"use client";
 
 const Hero = () => {
   return (
     <div className=" text-center min-h-screen flex justify-center  ">
       {/* <div className="hero_section_bg "></div> */}
-      <iframe
-        src="https://hero.hypefarm.io/"
-        width="100%"
-        frameborder="0"
-        height="auto"
-        className="hero_section_bg "
-      ></iframe>
-
+      <>
+        <iframe
+          src="https://hero.hypefarm.io/"
+          width="100%"
+          frameborder="0"
+          height="auto"
+          className="hero_section_bg "
+        ></iframe>
+      </>
       <div className=" hero_section_container   ">
         <div className=" mb-[4.25rem] sm:mb-[1.8rem] box-border  max-w-full">
           <h1 className=" text-5xl sm:text-3xl ">
@@ -19,14 +21,35 @@ const Hero = () => {
           </h1>
           <div className="text-5xl sm:text-3xl flex items-center justify-center ">
             For
-            <div className="hero-slider bg-secondary-500 ml-8 opacity-100">
-              <ul className="hero-slider__words opacity-100 marquee">
-                <li>Crypto</li>
+            <div className="hero-slider bg-primary-500 ml-8 opacity-70 rounded-lg ">
+              <marquee
+                direction="up"
+                width="100%"
+                height="100%"
+                behavior="scroll"
+                scrollamount="5"
+                onHover={() => stop()}
+              >
+                <ul className="hero-slider__words  marquee">
+                  <li>Crypto</li>
+                  <li>Startups</li>
+                  <li>B2B</li>
+                  <li>Software</li>
+                  <li>Fintech</li>
+                </ul>
+              </marquee>
+              {/* <ul className="hero-slider__words  marquee">
+                <li>
+                  <marquee direction="up">CRYPTO</marquee>
+                </li>
+                <li>
+                  <marquee direction="up">CRYPTO2</marquee>
+                </li>
                 <li>Startups</li>
                 <li>B2B</li>
                 <li>Software</li>
                 <li>Fintech</li>
-              </ul>
+              </ul> */}
             </div>
           </div>
         </div>
