ec3820261c139de921a2207439e3c79ad6a38463
sha <sheherazad.08@gmail.com>
2023-11-30 20:50:23 +0100

    add services new content

--- src/sections/services.jsx
+++ src/sections/services.jsx
@@ -11,10 +11,13 @@ const Services = () => {
             Our Digital <br /> Marketing Services
           </h1>
           <p className="  services-desc text-xl w-1/2 sm:w-full text-secondary-700">
-            We&apos;re experts in all things marketing. Business-to-business,
-            cryptocurrency, NFT, and high-growth startup brands are our sweet
-            spot. Discover how we can scale your business to new heights through
-            our strategic services below:
+            Welcome to Hypefarm, your dedicated partner in navigating the
+            dynamic landscape of Web3 marketing. As a leading web3 marketing
+            agency, we specialize in elevating businesses to new heights through
+            our strategic services. Whether you're a business-to-business
+            enterprise, a cryptocurrency pioneer, an NFT trailblazer, or a
+            high-growth startup brand, Hypefarm is here to amplify your presence
+            in the decentralized digital realm.
           </p>
         </div>
         <div className="  w-full flex-wrap justify-center align-center  grid grid-cols-3 ">
